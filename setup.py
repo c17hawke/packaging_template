067a31c0f340e9e4918d0fd224511e7417c87330
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-10 23:33:33 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "PROJECT_NAME_HERE"
+USER_NAME = "c17hawke"
+
+setuptools.setup(
+    name=f"example-pkg-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    # install_requires=[] # your dependent packages here
+)
